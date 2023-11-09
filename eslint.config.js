f28f826489d88d426de7fe99224c872343f76960
aibayanyu <aibayanyu@qq.com>
2023-11-10 07:10:03 +0800

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import mist from '@mistjs/eslint-config'
+
+export default mist({
+  rules: {
+    'no-console': 0,
+  },
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log()
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
