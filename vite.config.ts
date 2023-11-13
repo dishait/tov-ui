d6f21c254a658f6b90043bce1ab058f1a39adc4b
aibayanyu <aibayanyu@qq.com>
2023-11-13 21:23:46 +0800

    feat: add vitepress

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-console.log('s')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [],
 })
