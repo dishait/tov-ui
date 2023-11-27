85988d0e4cc20543219b0811ceeea48996deaedd
aibayanyu <aibayanyu@qq.com>
2023-11-28 07:49:42 +0800

    feat: add table header style

--- packages/tov-ui/src/table/header.tsx
+++ packages/tov-ui/src/table/header.tsx
@@ -1,14 +1,31 @@
 import { defineComponent } from 'vue'
+import { useClassname } from '@tov-ui/utils'
 import type { HeaderProps } from './typing'
 
-const Header = defineComponent<HeaderProps>(() => {
+const Header = defineComponent<HeaderProps>((props) => {
+  const { c } = useClassname('table-header')
   return () => {
+    const cls = {
+      [c()]: true,
+    }
+    const rowCls = {
+      [c('row')]: true,
+    }
+    const cellCls = {
+      [c('cell')]: true,
+    }
+    const { columns } = props
+
+    const renderColumn = () => {
+      return columns?.map((v) => {
+        return <th class={cellCls}>{v.title}</th>
+      })
+    }
+
     return (
-      <thead>
-        <tr>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
+      <thead class={cls}>
+        <tr class={rowCls}>
+          {renderColumn()}
         </tr>
       </thead>
     )
